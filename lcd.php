edba12d5d13e058cfc099b55432b9242e4f870ba
endry <itnayaz@gmail.com>
2019-06-30 19:37:40 +0700

    lcd layout update

--- lcd.php
+++ lcd.php
@@ -1,6 +1,6 @@
 <?php 
 error_reporting(0);
-
+include 'config/database.php';
  ?>
 <!DOCTYPE html>
 <!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
@@ -21,13 +21,12 @@ error_reporting(0);
 <meta name="author" content="">
 
 <!-- Le styles -->
-<link href="webroot/css/lib/bootstrap.css" rel="stylesheet">
-<link href="webroot/css/lib/bootstrap-responsive.css" rel="stylesheet">
-<link href="webroot/css/boo-extension.css" rel="stylesheet">
-<link href="webroot/css/boo_1.css" rel="stylesheet">
-<link href="webroot/css/style.css" rel="stylesheet">
-<link href="webroot/css/boo-coloring.css" rel="stylesheet">
-<link href="webroot/css/boo-utility.css" rel="stylesheet">
+<link href="webroot/css/custom_css/mylib/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="webroot/css/lib/bootstrap-responsive.css" rel="stylesheet"> -->
+<!-- <link href="webroot/css/boo-extension.css" rel="stylesheet"> -->
+<!-- <link href="webroot/css/boo_1.css" rel="stylesheet"> -->
+<!-- <link href="webroot/css/boo-coloring.css" rel="stylesheet"> -->
+<!-- <link href="webroot/css/boo-utility.css" rel="stylesheet"> -->
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
@@ -43,116 +42,82 @@ error_reporting(0);
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="webroot/ico/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="webroot/ico/apple-touch-icon-57-precomposed.png">
 <style> 
-                        .color { color:white; }  
-                        .blink { padding-top:20px; color:white; -webkit-animation: blink .75s linear infinite; -moz-animation: blink .75s linear infinite; -ms-animation: blink .75s linear infinite; -o-animation: blink .75s linear infinite; animation: blink .75s linear infinite; } @-webkit-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-moz-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-ms-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-o-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } 
-                    </style>
-  <script src="webroot/js/lib/jquery.js"></script>            
-             <link rel="stylesheet" href="compiled/flipclock.css">
+.color { color:white; }  
+.blink { padding-top:20px; color:white; -webkit-animation: blink .75s linear infinite; -moz-animation: blink .75s linear infinite; -ms-animation: blink .75s linear infinite; -o-animation: blink .75s linear infinite; animation: blink .75s linear infinite; } @-webkit-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-moz-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-ms-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @-o-keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } @keyframes blink { 0% { opacity: 1; } 50% { opacity: 1; } 50.01% { opacity: 0; } 100% { opacity: 0; } } 
+</style>
+<script src="webroot/js/lib/jquery.js"></script>            
+<!-- <link rel="stylesheet" href="compiled/flipclock.css"> -->
+<link href="webroot/css/style.css" rel="stylesheet">
 </head>
 
-<body class="sidebar-left ">
-<div class="page-container">
-    <div id="header-container">
-        <div id="header">
-            <div class="navbar navbar-inverse navbar-fixed-top">
-                <div class="navbar-inner">
-                    <div class="container-fluid">
-                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                        <a class="brand" href="javascript:void(0);"><h3>Antrian Poltekpel Sumbar</h3></a>
-		
+<body>
+    <div class="outer container">
+        <div class="row">
+            <div class="column list col-md-5">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="left box col-md-8">
+                            <div class="head">Nomor Antrian</div>
+                            <div class="content">A1</div>
+                        </div>
+                        <div class="right box col-md-4">
+                            <div class="head">LOKET</div>
+                            <div class="content">A</div>
+                        </div>
                     </div>
                 </div>
-                
-            </div>
-            <!-- // navbar -->
-            
-            
-            <!-- // drawer --> 
-        </div>
-    </div>
-    <!-- // header-container -->
-    
-    <div id="main-container">
-        
-        <?php include"content.php"?>
-        <?php
-        include 'config/database.php';
-        $detail = mysql_query("SELECT * FROM 
-                            video
-                            WHERE 
-                            id='1'");
-    $r    = mysql_fetch_array($detail); ?>
-
-        <div id="main-content" class="main-content container-fluid">
-            <div id="page-content" class="page-content">
-                <section>
-                    
-                    <div class="row-fluid">
-                        <div class="span12">
-                            <div id='media-player' class="span12">
-		<video id='media-video' class="span12" controls autoplay loop>
-                    <source src='video/<?php echo "$r[nama_file]";?>' type='video/mp4'>
-		</video>
-	</div>
-                           
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="left box col-md-8">
+                            <div class="head">Nomor Antrian</div>
+                            <div class="content">A1</div>
                         </div>
-                        <!-- // Column -->
-                        
+                        <div class="right box col-md-4">
+                            <div class="head">LOKET</div>
+                            <div class="content">B</div>
+                        </div>
                     </div>
-                    <!-- // Example row --> 
-                    
-                </section>
-                 <center> <h4 class="blink">.:: Budayakan Antri Untuk Kenyamanan Bersama, Terimakasih Atas Kunjungan Anda ::. </h4></center>    
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="left box col-md-8">
+                            <div class="head">Nomor Antrian</div>
+                            <div class="content">A1</div>
+                        </div>
+                        <div class="right box col-md-4">
+                            <div class="head">LOKET</div>
+                            <div class="content">C</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column col-md-7">
+                <div class="header">
+                    <h2>Antrian Poltekpel Sumbar</h2>
+                </div>
+                <div id='media-player' class="video-section col-md-12">
+                <?php
+                $detail = mysqli_query($connect,"SELECT * FROM video WHERE id='1'");
+                $r    = mysqli_fetch_array($detail); ?>
+                    <video id='media-video' class="col-md-12" controls autoplay loop>
+                        <source src='video/<?php echo "$r[nama_file]";?>' type='video/mp4'>
+                    </video>
+                </div>
+                <div class="static text-center"><h4>.:: Budayakan Antri Untuk Kenyamanan Bersama, Terimakasih Atas Kunjungan Anda ::. </h4></div>
             </div>
         </div>
-        <!-- // main-content --> 
-        
+        <div class="row">
+            <div class="running_text">
+                <marquee direction='left' scrollamount='5'>
+                    <?php 
+                        $sql = mysqli_query($connect, "SELECT  id, isi_text FROM running_text");
+                            while ($r=mysqli_fetch_array($sql)){
+                                echo $r[isi_text];
+                            }
+                    ?>
+                </marquee>
+            </div>
+        </div>
     </div>
-    <!-- // main-container  -->
-    
-    <footer id="footer-fix">
-        <div id="footer-sidebar" class="footer-sidebar">
-            <div class="navbar">
-                <!-- <center><div class="clock"></div></center> -->
-            </div>
-        </div>
-        <!-- // footer sidebar -->
-        
-        <div id="footer-content" class="footer-content">
-            <span style='font-family:arial; font-size: 15px; font-weight:bold; color:#FFFF00;'> <marquee direction='left' scrollamount='5' height='35px' width='100%' bgcolor='#272B2E' >
-            <?php 
-                $sql = mysql_query("SELECT  id, isi_text FROM running_text");
-
-                    while ($r=mysql_fetch_array($sql)){
-                        echo "$r[isi_text]";
-                    }
-            ?>
-                </marquee></span>
-
-            
-        </div>
-        <!-- // footer content --> 
-        
-    </footer>
-    <!-- // footer-fix  --> 
-    
-</div>
-<!-- // page-container  --> 
-
-<!-- Le javascript --> 
-<!-- Placed at the end of the document so the pages load faster --> 
-
-
-
-<script type="text/javascript">
-			var clock;
-			
-			$(document).ready(function() {
-				clock = $('.clock').FlipClock({
-					clockFace: 'TwelveHourClock'
-				});
-			});
-		</script>
-<script src="compiled/flipclock.js"></script>       
 </body>
 </html>
